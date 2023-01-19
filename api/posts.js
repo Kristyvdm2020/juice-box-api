8156a63f6cf773f7c85bee20673636c5ab405ed3
Kristyvdm2020 <kristyvdm2020@gmail.com>
2023-01-19 15:08:11 -0500

    added requireUser and endpoint to create posts

--- api/posts.js
+++ api/posts.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const postsRouter = express.Router();
 
+const { requireUser } = require('./utils');
+const { getAllPosts, createPost } = require('../db');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+    const { title, content, tags = "" } = req.body;
+  
+    const tagArr = tags.trim().split(/\s+/);
+    const postData = {};
+  
+    if (tagArr.length) {
+      postData.tags = tagArr;
+    }
+  
+    try {
+      postData.authorId = req.user.id;
+      postData.title = title;
+      postData.content = content;
+      const post = await createPost(postData);
+
+      if(post) {
+        res.send({ 
+            post
+          });
+      } 
+      
+    } catch ({ name, message }) {
+        next({ name, message });
+    }
+});
+
 postsRouter.use((req, res, next) => {
     console.log("A request is being made to /posts");
 
     next();
 });
-
-const { getAllPosts } = require('../db');
 
 postsRouter.get('/', async(req, res) => {
     const posts = await getAllPosts();
