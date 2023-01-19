929722d4c07d71e42818a1fd4a238a6c0bd877a3
Nolimitg7 <gsneed24@gmail.com>
2023-01-19 11:52:09 -0500

    added code to give users a token on login

--- api/users.js
+++ api/users.js
@@ -7,7 +7,7 @@ usersRouter.use((req, res, next) => {
   next();
 });
 
-const { getAllUsers } = require('../db');
+const { getAllUsers, getUserByUsername } = require('../db');
 
 usersRouter.get('/', async(req, res) => {
     const users = await getAllUsers();
@@ -16,5 +16,37 @@ usersRouter.get('/', async(req, res) => {
         users
     });
 });
+
+usersRouter.post('/login', async (req, res, next) => {
+  const { username, password } = req.body;
+  const jwt = require('jsonwebtoken');
+
+
+  // request must have both
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password"
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+    if (user && user.password == password) {
+      // create token & return to user
+      const token = jwt.sign({id:user.id, username:user.username},process.env.JWT_SECRET)
+      res.send({ message: "you're logged in!", token: `${token}` });
+    } else {
+      next({ 
+        name: 'IncorrectCredentialsError', 
+        message: 'Username or password is incorrect'
+      });
+    }
+  } catch(error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 
 module.exports = usersRouter;
